d0b3762b56b95d4e821ce6cd4695be3cefc0defd
wilder85 <juanalverto@gmail.com>
2017-01-30 14:39:36 -0600

    Weather App
    Weather App using a API

---
+++ app/src/main/java/com/belikeu/wilder/stormy/UI/AlertDialogFragment.java
@@ -0,0 +1,28 @@
+package com.belikeu.wilder.stormy.UI;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+import com.belikeu.wilder.stormy.R;
+
+/**
+ * Created by Wilder on 2/14/15.
+ */
+public class AlertDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle(context.getString(R.string.error_title))
+                .setMessage(context.getString(R.string.error_message))
+                .setPositiveButton(context.getString(R.string.error_ok_button_text), null);
+
+        AlertDialog dialog = builder.create();
+        return dialog; 
+
+    }
+}
---
+++ app/src/main/java/com/belikeu/wilder/stormy/weather/Forecast.java
@@ -0,0 +1,35 @@
+package com.belikeu.wilder.stormy.weather;
+
+/**
+ * Created by Wilder on 5/1/15.
+ */
+public class Forecast {
+
+        private Current mCurrent;
+        private Hour[] mHourlyForecast;
+        private Day[] mDailyForecast;
+
+    public Current getCurrent() {
+        return mCurrent;
+    }
+
+    public void setCurrent(Current current) {
+        mCurrent = current;
+    }
+
+    public Hour[] getHourlyForecast() {
+        return mHourlyForecast;
+    }
+
+    public void setHourlyForecast(Hour[] hourlyForecast) {
+        mHourlyForecast = hourlyForecast;
+    }
+
+    public Day[] getDailyForecast() {
+        return mDailyForecast;
+    }
+
+    public void setDailyForecast(Day[] dailyForecast) {
+        mDailyForecast = dailyForecast;
+    }
+}
